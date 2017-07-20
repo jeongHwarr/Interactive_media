f85a06a65b95145c2cfd2c66d354280ebb6ea0f4
Krlucete <Krlucete@gmail.com>
2017-07-20 09:08:17 +0900

    move source about db connection to main source

--- Jeonghwa/mouse_click_in_video/constants.php
+++ Jeonghwa/mouse_click_in_video/constants.php
@@ -1,10 +1,7 @@
-
- <?php
-
- $httproot = '.';
+<?php
+$httproot = '.';
 define ('HTTPROOT', $httproot);
  // define ('JSROOT', HTTPROOT.'/public/js');
 define ('CSSROOT', HTTPROOT.'/css');
  // define ('IMGROOT', HTTPROOT.'/public/img');
-
- ?>
+?>
