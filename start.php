fb8bc6168c830bb5c5839825b0052a9167c9cf82
prtscn <prtscn@donga.ac.kr>
2017-07-25 13:57:59 +0900

    ui add // add project start page (non DB)

---
+++ start.php
@@ -0,0 +1,73 @@
+<?php
+?>
+<!doctype html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <title>Project Start</title>
+</head>
+<body>
+<!--
+프로젝트 선택과 새로만드는 페이지
+-->
+    <div id= "contents_wrapper">
+
+        <button class="btn btn-default" id="new_project">new Project</button>
+        <div id ="project_list">
+            <ul>
+                <!--프로젝트 목록 추가-->
+                <li><a href=""target="_self">프로젝트 1</a></li>
+                <li><a href=""target="_self">프로젝트 2</li>
+                <li><a href=""target="_self">프로젝트 3</li>
+            </ul>
+
+        </div>
+        <div class="panel panel-default" id="video_select" style="display: none">
+            <div class="panel-heading">
+                <h3 class="panel-title">영상을 고르세요</h3>
+                <div class="panel-body">
+                    <!--영상목록 추가
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsvideo" id={변수} value={변수}>
+                            {영상 이름}
+                        </label>
+                    </div>
+                    -->
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsvideo" id="optionsvideo1" value="video1">
+                            영상 1번
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsvideo" id="optionsvideo2" value="video2">
+                            영상 2번
+                        </label>
+                    </div>
+
+                    <button class="btn btn-success">createProject</button>
+                </div>
+
+            </div>
+    </div>
+
+</body>
+<script>
+    $(document).ready(function(){
+        $("#new_project").click(function(){
+            $("#video_select").slideDown();
+        });
+    });
+</script>
+</html>
