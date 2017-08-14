e8a35195bf2d4934732b69250eebfc8ed87972bb
Krlucete <Krlucete@gmail.com>
2017-08-14 10:56:46 +0900

    전체화면으로 바뀔 때 파동효과의 크기를 변화시킴

--- assets/js/waves.js
+++ assets/js/waves.js
@@ -224,6 +224,11 @@ function setWaveEffect(v, box) {
        var scaleX = this.videoWidth / $(v).outerWidth();
        var scaleY = this.videoHeight / $(v).outerHeight();
 
+       //전체화면일때 크기조절
+       //Todo 전체화면으로 바뀔때 한번만 처리해줘도 되도록 변경필요
+       console.log($(v).outerWidth() * $(v).outerHeight()/138528);
+       scale = scale * Math.sqrt($(v).outerWidth() * $(v).outerHeight()/138528);
+
        //x, y 변환
        x = (x - 0.5)/scaleX;
        y = (y - 0.5)/scaleY;
