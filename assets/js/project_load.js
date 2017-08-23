bdd16949d8e93b6a809e39a4a98a277a275c45ff
Krlucete <Krlucete@gmail.com>
2017-08-23 15:40:05 +0900

    index.php 이름 변경

--- assets/js/project_load.js
+++ assets/js/project_load.js
@@ -10,7 +10,7 @@ function loadProject(project_id){
       session.set('waves_session', {['waves_session'] : data.waves});
       session.set('captions_session', {['captions_session'] : data.captions});
       session.set('stickers_session', {['stickers_session'] : data.stickers});
-      location.href = 'index.php?p_id='+data.project_info[0]['p_id'];
+      location.href = 'web_editor.php?p_id='+data.project_info[0]['p_id'];
       // ↑ query string은 사실 필요 없지만 프로젝트 혼동하지 않기 위해 사용. 나중에는 index.php로만 접근하도록 수정
     }
   })
